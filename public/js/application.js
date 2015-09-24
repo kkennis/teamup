d9b298bc59d2d1f64e075942c70dee9b544b80c9
kkennis <kevinvkennis@gmail.com>
2015-09-24 12:26:07 -0700

    Finish initial espn scraping

--- public/js/application.js
+++ public/js/application.js
@@ -1,18 +1,46 @@
 $(document).ready(function() {
-  var teams = [];
+  updateStories();
 
+});
+
+
+function updateStories(){
+  var team_names = [];
+  var team_classes = [];
+  
   $.ajax({
     url: "https://www.kimonolabs.com/api/dp2fr06w?apikey=RVGH7NXcr496gz5BGNjs805SWZvp2ATs",
     method: "GET",
     dataType: "jsonp"
   })
   .done(function(data){
-    team_names = [];
+    
     $.map(data["results"]["nflTeams"], function(team, index){
-      team_names.push(team["teamName"]["text"]);
+      team_names.push(team["teamName"]["href"]);
     });
-    console.log(team_names);
 
-
+    $.map(team_names, function(team_name, index){
+      var team_class = team_name.split("/").pop().replace(/-/g,"_");
+      if ($("div.stories").hasClass(team_class)){
+        getESPNStories(team_name);
+      };
+    });
   });
-});
+  
+}
+
+function getESPNStories(team_name){
+  var stories = $.ajax({
+    url: "https://www.kimonolabs.com/api/7ki3w3bq?apikey=RVGH7NXcr496gz5BGNjs805SWZvp2ATs",
+    method: "GET",
+    dataType: "jsonp"
+  })
+  .done(function(data){
+    console.log("We ajaxed!")
+    var teamStories = data["results"]["nflStories"].filter((story) => story.url == team_name);
+    $.map(teamStories.slice(0,5), function(story){
+      var team = story.url.split("/").pop().replace(/-/g,"_");
+      $("div." + team).append("<p>ESPN - <a href='" + story.title.href + "'>" + story.title.text + "</a></p><hr>");
+    })
+  })
+}
